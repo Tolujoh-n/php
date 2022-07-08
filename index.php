d4e251645302abc89fef0d80dbba2a5e9eef586c
olagbile john <104234518+tolujohnk@users.noreply.github.com>
2022-07-08 21:06:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<head><!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"><!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+ <title>olagbile john 350357bc</title>
+</head>
+<body>
+<div class="container">
+    <php 
+    echo()
+    ?>
+<h1>Please Log In</h1>
+<form method="POST">
+<label for="nam">User Name</label>
+<input type="text" name="who" id="nam"><br><label for="id_1723">Password</label>
+<input type="text" name="pass" id="id_1723"><br><input type="submit" value="Log In"><input type="submit" name="cancel" value="Cancel"></form>
+<p>
+For a password hint, view source and find a password hint
+in the HTML comments.
+<!-- Hint: The password is the three character name of the 
+programming language used in this class (all lower case) 
+followed by 123. -->
+</p>
+</div>
+</body>
